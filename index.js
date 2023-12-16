8eb7f2c796016ed1232d797d59ba8aa1c4080dd7
rahul <rkumar45650@gmail.com>
2023-12-16 14:53:46 +0530

    backend

---
+++ index.js
@@ -0,0 +1,26 @@
+const express=require("express")
+const app=express()
+const mongoose=require("mongoose")
+const myemp=require("./empModel/Empmodel")
+const cors=require("cors")
+const DB="mongodb+srv://rkumar45650:12345@cluster0.zhkyr3s.mongodb.net/mernstack?retryWrites=true&w=majority"
+mongoose.connect(DB).then(()=>{
+    console.log("connect")
+}).catch(()=>console.log("error"))
+    
+    app.use(express.json())
+    app.use(cors())
+
+
+
+
+app.get("/",(req,res)=>{
+    res.send("hello and welcome")
+})
+app.post("/save",myemp.newdata)
+
+
+
+app.listen(8000)
+
+
